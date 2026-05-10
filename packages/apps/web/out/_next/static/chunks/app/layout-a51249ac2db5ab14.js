d86b8151c4409535df7c065b9a409a6b261ce5e0
IvanYang <523166477@qq.com>
2026-05-11 00:51:21 +0800

    feat(M3): STORY-0038 Desktop 内嵌 Next.js Web UI

---
+++ packages/apps/web/out/_next/static/chunks/app/layout-a51249ac2db5ab14.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6079:()=>{},9825:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6079,23))}},e=>{e.O(0,[472,550,136,358],()=>e(e.s=9825)),_N_E=e.O()}]);
